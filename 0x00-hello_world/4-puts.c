96b898523091e5fe7e73bbeb7afc2f34c60403a4
Mussie09 <musatesfay42@gmail.com>
2022-10-13 07:37:08 -0700

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print with puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
