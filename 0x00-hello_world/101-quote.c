bfa43b7cbba14712d70224609b9fc2e4eb3b0a71
Mussie09 <musatesfay42@gmail.com>
2022-10-13 17:25:29 -0700

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing error
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(3, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
