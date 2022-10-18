6f560628beb8c22c95b65ad1d1d44771aa1fb35e
Mussie09 <musatesfay42@gmail.com>
2022-10-17 20:00:26 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * main - printing error
+ * main - entry point
  *
- * Return: Always 1 (success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(3, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+	write("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
