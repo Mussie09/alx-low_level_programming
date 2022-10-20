d74189976240450e03c143f38a8f867dc17e4153
Mussie09 <musatesfay42@gmail.com>
2022-10-20 06:37:12 -0700

    Nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * alphabets - prints alphabets in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char s;
+	{
+	for (s = 'a'; s <= 'z'; s++)
+		_putchar(s);
+	}
+	_putchar('\n');
+	return (0);
+}
