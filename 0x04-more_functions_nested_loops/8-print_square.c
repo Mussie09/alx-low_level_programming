04bd107df3e84a4b109af0a6ec690b66bedf2cb0
Mussie09 <musatesfay42@gmail.com>
2022-10-21 08:51:50 -0700

    More on nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_square - A function that prints a square
+ * print_square - A function that prints a square using the character #
  * @size: An input integer
  * followed by a new line
  * Return: Always 0
@@ -12,9 +12,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 0; j <= size; j++)
 				_putchar('#');
 			_putchar('\n');
 		}
