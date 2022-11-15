76c32fc94e1099d9f8baa92065d9083251bbefca
Mussie09 <musatesfay42@gmail.com>
2022-11-14 17:45:40 -0800

    preproccessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints the name of the line it was compiled in
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
