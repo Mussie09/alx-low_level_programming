2c460675cc8a609d0efaf80a6ada00d8fcc1a72a
Mussie09 <musatesfay42@gmail.com>
2022-11-18 18:49:51 -0800

    Hellow world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sunistd.h>
 
 /**
- * main - entry point
+ * main - print the size of various ty
  *
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
